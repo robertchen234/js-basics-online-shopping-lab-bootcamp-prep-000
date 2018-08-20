1bfb9183abf8580090c93cde01e057aedf1284fb
Robert Chen <robertchen234@gmail.com>
2018-08-20 00:11:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,17 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var itemInCart = false
+  for(var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart.splice(i, 1);
+    }
+  }
+  if (!itemInCart) {
+    console.log("That item is not in your cart.");
+  }
+  return cart
 }
 
 function placeOrder(cardNumber) {
